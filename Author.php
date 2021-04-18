4d1084c5cf0d378469e54de74fde02df08457af4
thanatos9516 <chepe352013@gmail.com>
2021-04-18 00:08:25 -0600

    Update Author.php

--- Author.php
+++ Author.php
@@ -4,16 +4,14 @@
 class Author extends Model {
 
   static $authors = [
-    ['id'=>4056,'name'=>'Juan Perez ','book'=>'C# .Net Developer',
-     'email'=>'juan.perez@univ.ac','publisher'=>'Editorial Costa Rica'], 
-     ['id'=>4056,'name'=>'Juan Perez ','book'=>'C# .Net Developer',
-     'email'=>'juan.perez@univ.ac','publisher'=>'Editorial Costa Rica'], 
-     ['id'=>4056,'name'=>'Juan Perez ','book'=>'C# .Net Developer',
-     'email'=>'juan.perez@univ.ac','publisher'=>'Editorial Costa Rica'], 
-     ['id'=>4056,'name'=>'Juan Perez ','book'=>'C# .Net Developer',
-     'email'=>'juan.perez@univ.ac','publisher'=>'Editorial Costa Rica'], 
-     ['id'=>4056,'name'=>'Juan Perez ','book'=>'C# .Net Developer',
-     'email'=>'juan.perez@univ.ac','publisher'=>'Editorial Costa Rica'], 
+    ['id'=>4056,'name'=>'Abraham Silberschatz ','book'=>'Operating System Concepts',
+     'email'=>'juan.perez@univ.ac','publisher'=>'John Wiley & Sons'], 
+     ['id'=>4056,'name'=>'Abraham Silberschatz ','book'=>'Database System Concepts',
+     'email'=>'juan.perez@univ.ac','publisher'=>'John Wiley & Sons'], 
+     ['id'=>4056,'name'=>' 	Andrew S. Tanenbaum ','book'=>'Computer Networks',
+     'email'=>'juan.perez@univ.ac','publisher'=>'Pearson Education 	'], 
+     ['id'=>4056,'name'=>' 	Andrew S. Tanenbaum ','book'=>'Modern Operating Systems',
+     'email'=>'juan.perez@univ.ac','publisher'=>'Pearson Education 	'], 
   ];
 
   public static function all() { 
